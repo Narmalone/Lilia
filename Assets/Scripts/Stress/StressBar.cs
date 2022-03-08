128f9d1217e9f68fa5d636654f75328651892e30
Narmalone <62171923+Narmalone@users.noreply.github.com>
2022-03-08 20:03:32 +0100

    Ajout du stress simple

---
+++ Assets/Scripts/Stress/StressBar.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StressBar : MonoBehaviour
+{
+    [SerializeField, Tooltip("Stress actuel du personnage")]private float m_currentStress;
+    [SerializeField, Tooltip("Augmentation du stress du joueur")] private float m_upgradeStress;
+
+    private bool isCinematic;
+
+    private void Start()
+    {
+        isCinematic = false;
+    }
+    private void Update()
+    {
+        if (isCinematic == true)
+        {
+            m_upgradeStress = 0;
+        }
+        else
+        {
+            m_currentStress += m_upgradeStress;
+            m_upgradeStress = 0.001f;
+        }
+        Debug.Log(m_currentStress);
+    }
+}
