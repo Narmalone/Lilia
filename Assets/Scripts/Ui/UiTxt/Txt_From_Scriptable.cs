1559b25d128cefd98c06d12a65b2bb6461e9139e
maxime <62171923+Narmalone@users.noreply.github.com>
2022-04-15 15:54:16 +0200

    ajout changement de langue (pas complété)

--- Assets/Scripts/Ui/UiTxt/Txt_From_Scriptable.cs
+++ Assets/Scripts/Ui/UiTxt/Txt_From_Scriptable.cs
@@ -11,9 +11,28 @@ public class Txt_From_Scriptable : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI m_txtToModify;
 
+    private void Awake()
+    {
+        m_txtToModify = GetComponent<TextMeshProUGUI>();
+    }
 
     public void OnChangeLanguage()
     {
-        
+        if (m_txtLanguage.index == 0)
+        {
+            ClearText();
+            m_txtToModify.text = m_txtLanguage.m_Sentence[0];
+        }
+        else if (m_txtLanguage.index == 1)
+        {
+            ClearText();
+            m_txtToModify.text = m_txtLanguage.m_Sentence[1];
+        }
     }
+
+    public void ClearText()
+    {
+        m_txtToModify.text = String.Empty;
+    }
+
 }
