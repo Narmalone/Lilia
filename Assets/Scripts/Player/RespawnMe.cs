ee61ab5860e6dc91d06203229f3637fe14705a8f
Narmalone <62171923+Narmalone@users.noreply.github.com>
2022-05-29 17:07:19 +0200

    léger fix du respawn

--- Assets/Scripts/Player/RespawnMe.cs
+++ Assets/Scripts/Player/RespawnMe.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class RespawnMe : MonoBehaviour
 {
     [SerializeField] PlayerController m_player;
+    [SerializeField] Doudou m_doudou;
+    [SerializeField] FlashlightManager m_flm;
+    [SerializeField] UiManager m_uiManager;
     [Header("Objects Transform"), Space(10)]
-    [SerializeField] public Transform m_doudoutransform;
     [SerializeField] public Transform m_playertransform;
     [SerializeField] public Transform m_iaTransform;
-    [SerializeField] public Transform m_veilleuseTransform;
 
     [Header("Checkpoints_1"), Space(10)]
     [SerializeField] public Transform m_playercheckpoints;
-    [SerializeField] public Transform m_doudoucheckpoint;
     [SerializeField] public Transform m_iaCheckpoint;
-    [SerializeField] public Transform m_veilleuseCheckPoint;
 
     [SerializeField] private MenuManager m_menuManager;
 
@@ -29,7 +28,6 @@ public class RespawnMe : MonoBehaviour
         makeRespawn = true;
         m_gameManager = FindObjectOfType<GameManager>();
         index = 0;
-       
     }
 
     private void OnEnable()
@@ -42,17 +40,12 @@ public class RespawnMe : MonoBehaviour
     }
     private void LateUpdate()
     {
-        //if (Input.GetKeyDown(KeyCode.T))
-        //{
-            //if (makeRespawn == true)
-            //{
-                //Respawn();
-            //}
-        //}  
         if (m_gameManager.isDead == true)
         {
             if (Input.anyKeyDown)
             {
+                m_iaTransform.position = m_iaCheckpoint.position;
+                m_player.transform.position = m_playercheckpoints.transform.position;
                 Respawn();
                 Debug.Log("mort et appuie sur une touche");
             }
@@ -67,19 +60,22 @@ public class RespawnMe : MonoBehaviour
     }
     public void Respawn()
     {
-        m_player.transform.position = m_playercheckpoints.transform.position;
-
-        m_iaTransform.position = m_iaCheckpoint.position;
 
         if(m_player.m_doudouIsPossessed == false)
         {
-            m_doudoutransform.transform.position = m_doudoucheckpoint.transform.position;
-            Debug.Log("joueur a pas le doudou donc tp");
+            m_uiManager.TakeDoudou();
+            m_doudou.PickItem();
+            m_player.m_doudouIsPossessed = true;
+            Debug.Log("joueur a pas le doudou donc l'attribuer au joueur");
         }
 
         if(m_player.m_flashlightIsPossessed == false)
         {
-            m_veilleuseTransform.position = m_veilleuseCheckPoint.position;
+            m_uiManager.TakeLampe();
+            m_flm.PickItem();
+            m_player.m_flashlightIsPossessed = true;
+            //m_veilleuseTransform.position = m_veilleuseCheckPoint.position;
+            Debug.Log("joueur a pas la veilleuse donc l'attribuer au joueur");
         }
 
         m_menuManager.OnRespawn();
